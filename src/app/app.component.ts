eb6768411e13a8dcc0450cb903c540b456ad72d8
Freja Fischer <frej657e@stud.kea.dk>
2024-02-05 09:30:59 +0100

    added service and component folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { ThemeToggleComponent } from './components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
